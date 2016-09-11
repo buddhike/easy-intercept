2641be68160931469ba1c9e0a18bb273781b06f5
Buddhike de Silva <buddhike.desilva@geeksdiary.com>
2016-09-12 06:44:54 +1000

    Refactored tests.

--- test/interceptor.js
+++ test/interceptor.js
@@ -1,13 +1,4 @@
 import interceptor from '../lib/interceptor';
-import R from 'ramda';
-
-const output = f => {
-  let o;
-
-  f.map(v => { o = v; });
-
-  return o;
-};
 
 describe('interceptor', () => {
   const call = { index: 0 }
@@ -15,21 +6,21 @@ describe('interceptor', () => {
   it('should return nothing by default', () => {
     const i = interceptor();
 
-    (!output(i.intercept())).should.be.true;
+    i.intercept().getOrElse(false).should.be.false;
   });
 
   describe('onCall', () => {
     it('should return the pre-configured value', () => {
       const i = interceptor().onCall(0, 42);
-      const t = R.compose(output, i.intercept);
-      t(call).should.equal(42);
+
+      i.intercept(call).getOrElse(false).should.equal(42);
     });
 
     describe('chained multiple times', () => {
       it('should return the last value', () => {
         const i = interceptor().onCall(0)(42).onCall(0)(43);
 
-        output(i.intercept({ index: 0 })).should.equal(43);
+        i.intercept({ index: 0 }).getOrElse(false).should.equal(43);
       });
     });
 
@@ -37,7 +28,7 @@ describe('interceptor', () => {
       it('should return the matching value', () => {
         const i = interceptor().onCall(0)(42).onCall(1)(43);
 
-        output(i.intercept({ index: 1 })).should.equal(43);
+        i.intercept({ index: 1 }).getOrElse(false).should.equal(43);
       });
     });
   });
@@ -46,10 +37,8 @@ describe('interceptor', () => {
     describe('matching all args', () => {
       it('should return the configured value', () => {
         const i = interceptor().withArgs(['a', 'b'])(42);
-        const r = i.intercept({ args: ['a', 'b'] });
-        const result = output(r) || false;
 
-        result.should.equal(42);
+        i.intercept({ args: ['a', 'b'] }).getOrElse(false).should.equal(42);
       });
     });
 
@@ -59,10 +48,7 @@ describe('interceptor', () => {
           .withArgs(['a' ])(42)
           .withArgs(['a'])(43);
 
-        const r = i.intercept({ args: ['a'] });
-        const result = output(r) || false;
-
-        result.should.equal(43);
+        i.intercept({ args: ['a'] }).getOrElse(false).should.equal(43);
       })
     });
 
@@ -70,28 +56,23 @@ describe('interceptor', () => {
       it('should not return the value for calls with arguments', () => {
         const i = interceptor().withArgs([])(42);
 
-        const result = output(i.intercept( { args: ['a'] })) || false;
-        result.should.be.false;
+        i.intercept( { args: ['a'] }).getOrElse(false).should.be.false;
       });
     });
 
     describe('match some args', () => {
       it('should return the configured value', () => {
         const i = interceptor().withArgs(['a'])(42);
-        const r = i.intercept({ args: [ 'a', 'b' ] });
-        const result = output(r) || false;
 
-        result.should.equal(42);
+        i.intercept({ args: [ 'a', 'b' ] }).getOrElse(false).should.equal(42);
       });
     });
 
     describe('match some args out of order', () => {
       it('should not return the configured value', () => {
         const i = interceptor().withArgs(['a', 'b'])(42);
-        const r = i.intercept({ args: [ 'c', 'b' ] });
-        const result = output(r) || false;
 
-        result.should.be.false;
+        i.intercept({ args: [ 'c', 'b' ] }).getOrElse(false).should.be.false;
       });
     });
   });
