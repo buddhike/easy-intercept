0c2a71c05075b87643e0bc38bc1c2c12d55d735d
Buddhike de Silva <buddhike.desilva@geeksdiary.com>
2016-09-25 12:09:35 +1000

    Refactored tests.

--- test/interceptor.js
+++ test/interceptor.js
@@ -1,79 +1,77 @@
 import interceptor from '../lib/interceptor';
 
-describe('interceptor', () => {
-  const call = { index: 0, args: [] }
+const call = { index: 0, args: [] }
 
+describe('An Interceptor', () => {
   it('should return nothing by default', () => {
-    const i = interceptor();
+    interceptor().intercept().getOrElse(false).should.be.false;
+  });
+});
 
-    i.intercept().getOrElse(false).should.be.false;
+describe('Interceptor onCall configuration', () => {
+  it('should return the pre-configured value', () => {
+    const i = interceptor().onCall(0, 42);
+
+    i.intercept(call).getOrElse(false).should.equal(42);
   });
 
-  describe('onCall', () => {
-    it('should return the pre-configured value', () => {
-      const i = interceptor().onCall(0, 42);
+  describe('chained multiple times', () => {
+    it('should return the last value', () => {
+      const i = interceptor().onCall(0, 42).onCall(0, 43);
 
-      i.intercept(call).getOrElse(false).should.equal(42);
-    });
-
-    describe('chained multiple times', () => {
-      it('should return the last value', () => {
-        const i = interceptor().onCall(0, 42).onCall(0, 43);
-
-        i.intercept({ index: 0, args: [] }).getOrElse(false).should.equal(43);
-      });
-    });
-
-    describe('mixed configurations', () => {
-      it('should return the matching value', () => {
-        const i = interceptor().onCall(0, 42).onCall(1, 43);
-
-        i.intercept({ index: 1, args: [] }).getOrElse(false).should.equal(43);
-      });
+      i.intercept({ index: 0, args: [] }).getOrElse(false).should.equal(43);
     });
   });
 
-  describe('withArgs', () => {
-    describe('matching all args', () => {
-      it('should return the configured value', () => {
-        const i = interceptor().withArgs(['a', 'b'], 42);
+  describe('for different invocations', () => {
+    it('should return the matching value', () => {
+      const i = interceptor().onCall(0, 42).onCall(1, 43);
 
-        i.intercept({ args: ['a', 'b'] }).getOrElse(false).should.equal(42);
-      });
+      i.intercept({ index: 1, args: [] }).getOrElse(false).should.equal(43);
     });
+  });
+});
 
-    describe('chained multiple times', () => {
-      it('should return the last value', () => {
-        const i = interceptor()
-          .withArgs([ 'a' ], 42)
-          .withArgs([ 'a' ], 43);
+describe('Interceptor withArgs configuration', () => {
+  describe('matching all args', () => {
+    it('should return the configured value', () => {
+      const i = interceptor().withArgs(['a', 'b'], 42);
 
-        i.intercept({ args: ['a'] }).getOrElse(false).should.equal(43);
-      })
+      i.intercept({ args: ['a', 'b'] }).getOrElse(false).should.equal(42);
     });
+  });
 
-    describe('match no arguments', () => {
-      it('should not return the value for calls with arguments', () => {
-        const i = interceptor().withArgs([], 42);
+  describe('chained multiple times', () => {
+    it('should return the last value', () => {
+      const i = interceptor()
+      .withArgs([ 'a' ], 42)
+      .withArgs([ 'a' ], 43);
 
-        i.intercept( { args: ['a'] }).getOrElse(false).should.be.false;
-      });
+      i.intercept({ args: ['a'] }).getOrElse(false).should.equal(43);
     });
+  });
 
-    describe('match some args', () => {
-      it('should return the configured value', () => {
-        const i = interceptor().withArgs(['a'], 42);
+  describe('match no arguments', () => {
+    it('should not return the value for calls with arguments', () => {
+      const i = interceptor().withArgs([], 42);
 
-        i.intercept({ args: [ 'a', 'b' ] }).getOrElse(false).should.equal(42);
-      });
+      i.intercept( { args: ['a'] }).getOrElse(false).should.be.false;
     });
+  });
 
-    describe('match some args out of order', () => {
-      it('should not return the configured value', () => {
-        const i = interceptor().withArgs(['a', 'b'], 42);
+  describe('match some args', () => {
+    it('should return the configured value', () => {
+      const i = interceptor().withArgs(['a'], 42);
 
-        i.intercept({ args: [ 'c', 'b' ] }).getOrElse(false).should.be.false;
-      });
+      i.intercept({ args: [ 'a', 'b' ] }).getOrElse(false).should.equal(42);
+    });
+  });
+
+  describe('match some args out of order', () => {
+    it('should not return the configured value', () => {
+      const i = interceptor().withArgs(['a', 'b'], 42);
+
+      i.intercept({ args: [ 'c', 'b' ] }).getOrElse(false).should.be.false;
     });
   });
 });
--- test/proxy.js
+++ test/proxy.js
@@ -1,280 +1,300 @@
 import R from 'ramda';
 import proxy from '../lib/proxy';
 
-describe('proxy', () => {
-  describe('to a function', () => {
-    let proxyFunction;
+describe('Proxy to a function', () => {
+  it('should invoke that function', () => {
+    const f = proxy(() => 42);
+    f().should.equal(42);
+  });
+});
 
+describe('Proxy to a function with args', () => {
+  it('should pipe the args', () => {
+    const f = proxy(a => a);
+    f(42).should.equal(42);
+  });
+});
+
+describe('Proxy that returns specific value for matching input', () => {
+  let f;
+
+  beforeEach(() => {
+    f = proxy(a => a).withArgs(42).returns(43);
+  })
+
+  it('should return the configured value for matching input', () => {
+    f(42).should.equal(43);
+  });
+
+  it('should return the original value for rest', () => {
+    f(1).should.equal(1);
+  });
+});
+
+describe('Proxy that throws an error for specific args', () => {
+  let target;
+
+  beforeEach(() => {
+    target = proxy(a => a);
+    target.withArgs(42).throws(new Error('doh'));
+  });
+
+  it('should throw specified error for that call', () => {
+    (() => target(42)).should.throw(/doh/);
+  });
+
+  it('should not throw an error for other calls', () => {
+    target(1).should.equal(1);
+  });
+});
+
+describe('Proxy that returns a specific value on specific call', () => {
+  let target;
+
+  beforeEach(() => {
+    target = proxy(() => 42);
+    target.onCall(0).returns(1);
+  });
+
+  it('should return the configured value for that call', () => {
+    target().should.equal(1);
+  });
+
+  it('should return original value for rest', () => {
+    target();
+    target().should.equal(42);
+  });
+});
+
+describe('Proxy that throws on specific call', () => {
+  let target;
+
+  beforeEach(() => {
+    target = proxy(a => a);
+    target.onCall(0).throws(new Error('doh'));
+  });
+
+  it('should throw specified error for that call', () => {
+    (() => target()).should.throw(/doh/);
+  });
+
+  it('should not throw an error for other calls', () => {
+    const firstCall = R.tryCatch(target, R.F);
+
+    firstCall();
+    target(1).should.equal(1);
+  });
+});
+
+describe('Proxy with a specific return value', () => {
+  let target;
+
+  beforeEach(() => {
+    target = proxy(() => 42);
+    target.returns(43);
+  });
+
+  it('should return the configured value', () => {
+    target().should.equal(43);
+  });
+
+  describe('and a specific return value on specific call', () => {
     beforeEach(() => {
-      proxyFunction = proxy(() => 42);
+      target.onCall(0).returns(44);
     });
 
-    it('should invoke that function', () => {
-      proxyFunction().should.equal(42);
+    it('should return the configured value for matching call', () => {
+      target().should.equal(44);
+    });
+
+    it('should return the original value for rest', () => {
+      target();
+      target().should.equal(43);
     });
   });
 
-  describe('to a function with args', () => {
-    let target;
-
+  describe('and a specific return value for matching input', () => {
     beforeEach(() => {
-      target = proxy(a => a);
+      target.withArgs(0).returns(44);
     });
 
-    it('should pipe the args', () => {
-      target(42).should.equal(42);
+    it('should return the configured value for matching call', () => {
+      target(0).should.equal(44);
     });
 
-    describe('with specific return value for input', () => {
-      beforeEach(() => {
-        target.withArgs(42).returns(43);
-      });
-
-      it('should return the configured value for matching input', () => {
-        target(42).should.equal(43);
-      });
+    it('should return the original value for rest', () => {
+      target(1).should.equal(43);
     });
   });
+});
 
-  describe('throwing on specific call', () => {
+describe('Accessing call information', () => {
+  let target;
+
+  beforeEach(() => {
+    target = proxy(() => 42);
+    target('a');
+    target('b');
+  });
+
+  it('should return arguments used for specified invocation', () => {
+    target.call(0).args[0].should.equal('a');
+    target.call(1).args[0].should.equal('b');
+  });
+
+  it('should return undefined for invalid calls', () => {
+    (target.call(2) || false).should.be.false;
+  });
+});
+
+describe('Verifying a received call', () => {
+  let target;
+
+  beforeEach(() => {
+    target = proxy(() => 42);
+    target('a');
+  });
+
+  it('should return matching calls', () => {
+    target.received('a')[0].args[0].should.equal('a');
+  });
+
+  it('should return false for non-matching calls', () => {
+    target.received('b').should.be.false;
+  });
+
+  describe('for multiple invocations', () => {
+    it('should return all matching calls', () => {
+      target('a');
+      target.received('a').length.should.equal(2);
+    });
+  });
+});
+
+describe('receivedAny', () => {
+  let target;
+
+  beforeEach(() => {
+    target = proxy(() => 42);
+    target('a');
+  });
+
+  it('should return true for matching calls', () => {
+    target.receivedAny('a').should.be.true;
+  });
+
+  it('should return false for non-matching calls', () => {
+    target.receivedAny('b').should.be.false;
+  });
+});
+
+describe('quick calls', () => {
+  let target;
+
+  beforeEach(() => {
+    target = proxy(() => 42);
+    target('a');
+    target('b');
+    target('c');
+  });
+
+  it('should include the first call', () => {
+    target.firstCall().args[0].should.equal('a');
+  });
+
+  it('should include the second call', () => {
+    target.secondCall().args[0].should.equal('b');
+  });
+
+  it('should include the third call', () => {
+    target.thirdCall().args[0].should.equal('c');
+  });
+});
+
+describe('throws', () => {
+  let target;
+
+  beforeEach(() => {
+    target = proxy(() => 42);
+    target.throws(new Error('doh'));
+  });
+
+  it('should throw the specified error', () => {
+    (() => target()).should.throw(/doh/);
+  });
+});
+
+describe('with an alternative implementation', () => {
+  it('should receive arguments', () => {
+    const target = proxy(() => 42);
+    target.onCall(0).returns(a => a);
+    target(43).should.equal(43);
+  });
+
+  it('should provide call info as the last argument', () => {
+    const target = proxy(() => 42);
+
+    target.onCall(0).returns((input, call) => {
+      input.should.equal('a');
+      call.count.should.equal(1);
+    });
+
+    target('a');
+  });
+
+  describe('for specific call', () => {
     let target;
 
     beforeEach(() => {
       target = proxy(() => 42);
-      target.onCall(0).throws(new Error('doh'));
+      target.onCall(0).returns(() => 43);
     });
 
-    it('should throw specified error for that call', () => {
-      (() => target()).should.throw(/doh/);
+    it('should invoke the alternative impl for matching call', () => {
+      target().should.equal(43);
     });
 
-    it('should not throw an error for other calls', () => {
-      const firstCall = R.tryCatch(target, R.F);
-
-      firstCall();
+    it('should not invoke the alternative impl for other calls', () => {
+      target();
       target().should.equal(42);
     });
   });
 
-  describe('throwing on specific args', () => {
+  describe('for call with specific args', () => {
     let target;
 
     beforeEach(() => {
       target = proxy(() => 42);
-      target.withArgs('a').throws(new Error('doh'));
+      target.withArgs('a').returns(() => 43);
     });
 
-    it('should throw an error for matching args', () => {
-      (() => target('a')).should.throw(/doh/);
+    it('should invoke the alternative impl for matching calls', () => {
+      target('a').should.equal(43);
     });
 
-    it('should not throw an error for other calls', () => {
+    it('should not invoke the alternative impl for other calls', () => {
       target('b').should.equal(42);
     });
   });
 
-  describe('configure value on call', () => {
-    let proxyFunction;
+  describe('accessing this from an arrow function', () => {
+    function Foo() {
+      this._state = 0;
 
-    beforeEach(() => {
-      proxyFunction = proxy(() => 42);
-      proxyFunction.onCall(0).returns(1);
-    });
+      this.inc = () => {
+        return this._state++;
+      };
 
-    it('should return the configured value for that call', () => {
-      proxyFunction().should.equal(1);
-    });
-  });
-
-  describe('configure a return value', () => {
-    let proxyFunction;
-
-    beforeEach(() => {
-      proxyFunction = proxy(() => 42);
-      proxyFunction.returns(43);
-    });
-
-    it('should return the configured value', () => {
-      proxyFunction().should.equal(43);
-    });
-
-    describe('with a specific call configuration', () => {
-      beforeEach(() => {
-        proxyFunction.onCall(0).returns(44);
-      });
-
-      it('should return call specific value for that call', () => {
-        proxyFunction().should.equal(44);
-      });
-
-      it('should return the default value of all other calls', () => {
-        proxyFunction();
-        proxyFunction().should.equal(43);
-      });
-    });
-  });
-
-  describe('call', () => {
-    let proxyFunction;
-
-    beforeEach(() => {
-      proxyFunction = proxy(() => 42);
-    });
-
-    it('should provide access to arguments', () => {
-      proxyFunction('a');
-      proxyFunction('b');
-      proxyFunction.call(0).args[0].should.equal('a');
-      proxyFunction.call(1).args[0].should.equal('b');
-    });
-
-    it('should return undefined for invalid calls', () => {
-      (proxyFunction.call(0) || false).should.be.false;
-    });
-  });
-
-  describe('received', () => {
-    let proxyFunction;
-
-    beforeEach(() => {
-      proxyFunction = proxy(() => 42);
-      proxyFunction('a');
-    });
-
-    it('should return matching calls', () => {
-      proxyFunction.received('a')[0].args[0].should.equal('a');
-    });
-
-    it('should return false for non-matching calls', () => {
-      proxyFunction.received('b').should.be.false;
-    });
-  });
-
-  describe('receivedAny', () => {
-    let proxyFunction;
-
-    beforeEach(() => {
-      proxyFunction = proxy(() => 42);
-      proxyFunction('a');
-    });
-
-    it('should return true for matching calls', () => {
-      proxyFunction.receivedAny('a').should.be.true;
-    });
-
-    it('should return false for non-matching calls', () => {
-      proxyFunction.receivedAny('b').should.be.false;
-    });
-  });
-
-  describe('quick calls', () => {
-    let proxyFunction;
-
-    beforeEach(() => {
-      proxyFunction = proxy(() => 42);
-      proxyFunction('a');
-      proxyFunction('b');
-      proxyFunction('c');
-    });
-
-    it('should include the first call', () => {
-      proxyFunction.firstCall().args[0].should.equal('a');
-    });
-
-    it('should include the second call', () => {
-      proxyFunction.secondCall().args[0].should.equal('b');
-    });
-
-    it('should include the third call', () => {
-      proxyFunction.thirdCall().args[0].should.equal('c');
-    });
-  });
-
-  describe('throws', () => {
-    let proxyFunction;
-
-    beforeEach(() => {
-      proxyFunction = proxy(() => 42);
-      proxyFunction.throws(new Error('doh'));
-    });
-
-    it('should throw the specified error', () => {
-      (() => proxyFunction()).should.throw(/doh/);
-    });
-  });
-
-  describe('with an alternative implementation', () => {
-    it('should receive arguments', () => {
-      const target = proxy(() => 42);
-      target.onCall(0).returns(a => a);
-      target(43).should.equal(43);
-    });
-
-    it('should provide call info as the last argument', () => {
-      const target = proxy(() => 42);
-
-      target.onCall(0).returns((input, call) => {
-        input.should.equal('a');
-        call.count.should.equal(1);
-      });
-
-      target('a');
-    });
-
-    describe('for specific call', () => {
-      let target;
-
-      beforeEach(() => {
-        target = proxy(() => 42);
-        target.onCall(0).returns(() => 43);
-      });
-
-      it('should invoke the alternative impl for matching call', () => {
-        target().should.equal(43);
-      });
-
-      it('should not invoke the alternative impl for other calls', () => {
-        target();
-        target().should.equal(42);
-      });
-    });
-
-    describe('for call with specific args', () => {
-      let target;
-
-      beforeEach(() => {
-        target = proxy(() => 42);
-        target.withArgs('a').returns(() => 43);
-      });
-
-      it('should invoke the alternative impl for matching calls', () => {
-        target('a').should.equal(43);
-      });
-
-      it('should not invoke the alternative impl for other calls', () => {
-        target('b').should.equal(42);
-      });
-    });
-
-    describe('accessing this from an arrow function', () => {
-      function Foo() {
-        this._state = 0;
-
-        this.inc = () => {
-          return this._state++;
-        };
-
-        this.invokeProxy = () => {
-          const p = proxy(() => this.inc() );
-          return p();
-        }
+      this.invokeProxy = () => {
+        const p = proxy(() => this.inc() );
+        return p();
       }
+    }
 
-      it('should provide the correct reference', () => {
-        const f = new Foo();
-        f.invokeProxy();
-        f.invokeProxy().should.equal(1);
-      });
+    it('should provide the correct reference', () => {
+      const f = new Foo();
+      f.invokeProxy();
+      f.invokeProxy().should.equal(1);
     });
   });
 });
